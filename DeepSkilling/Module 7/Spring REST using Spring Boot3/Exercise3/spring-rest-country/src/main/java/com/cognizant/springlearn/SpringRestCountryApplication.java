33474d01ef45d6a57c0ee5a4b32e7255ed0a746b
Suraj <pothuganti.suraj06@gmail.com>
2026-07-06 13:02:05 +0530

    Spring REST exercises

---
+++ DeepSkilling/Module 7/Spring REST using Spring Boot3/Exercise3/spring-rest-country/src/main/java/com/cognizant/springlearn/SpringRestCountryApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCountryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestCountryApplication.class, args);
+    }
+}
